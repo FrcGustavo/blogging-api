c54f23912e42affca37d52da722c134c8e680f63
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-14 01:17:59 -0500

    add styles

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+const path = require('path');
 const express = require('express');
 
 const ROUTER = require('./router');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', './src/views');
 app.set('view engine', 'pug');
 app.use(express.urlencoded({ extended: false }));
