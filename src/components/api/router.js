967ad471a1904c9bc6e1a2a155d66934e3fc31ae
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-14 18:06:31 -0500

    Add middlewares of session user

--- src/components/api/router.js
+++ src/components/api/router.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const isValidSessionUser = require('../../utils/middlewares/isValidSessionUser');
+const isLogged = require('../../utils/middlewares/isLogged');
 
 const controller = require('./controller');
 
@@ -6,11 +8,11 @@ const API = (app) => {
   const router = express.Router();
   app.use('/', router);
 
-  router.get('/', controller.index);
-  router.get('/signup', controller.signup);
-  router.post('/signup', controller.register);
-  router.get('/signin', controller.signin);
-  router.post('/signin', controller.login);
+  router.get('/', isValidSessionUser('/signin'), controller.index);
+  router.get('/signup', isLogged, controller.signup);
+  router.post('/signup', isLogged, controller.register);
+  router.get('/signin', isLogged, controller.signin);
+  router.post('/signin', isLogged, controller.login);
 };
 
 module.exports = API;
