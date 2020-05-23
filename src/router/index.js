96aaf8224bbc38ff57026a2d4a440d2e2646d306
FrcGustavo <hidalgo_fco520@outlook.com>
2020-05-23 03:47:09 -0500

    👷‍♂️[Add] files at the router

--- src/router/index.js
+++
@@ -1,9 +0,0 @@
-const API = require('../components/api/router');
-const POSTS = require('../components/posts/router');
-
-const ROUTER = (app) => {
-  API(app);
-  POSTS(app);
-};
-
-module.exports = ROUTER;
