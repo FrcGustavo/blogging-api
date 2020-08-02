fa4652aaae367a31923935142366f1e13312a5b6
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-02 01:02:48 -0500

    👨‍🔧[Add] refactor to PostsController

--- src/components/posts/index.ts
+++ src/components/posts/index.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 import passport from 'passport';
 import PostsRouter from './router';
+import PostsController from './controller';
+import Post from '../../models/posts'
+import success from '../../router/success';
 
 const POST = (app: any) => {
     const router = Router();
     const service =
-    const controller = 
+    const controller = PostsController(service, success);
 
     app.use('/api/posts', router);
     PostsRouter(router, controller, passport);
