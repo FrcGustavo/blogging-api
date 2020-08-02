bd0d70a3b893a5ae6c85792a10579ad4410b21e0
Francisco Gustavo <hidalgo_fco520@outlook.com>
2020-08-02 03:23:29 -0500

    👨‍🔧[Add] test to PostsService method findByAuthor

--- src/__tests__/components/posts/service.posts.test.ts
+++ src/__tests__/components/posts/service.posts.test.ts
@@ -1,5 +1,5 @@
 import PostsService from '../../../components/posts/service';
-import fakeModelPosts, { mockPostsLits } from '../../../utils/fakeModels/fakePosts';
+import fakeModelPosts, { mockPostsLits, mockPost } from '../../../utils/fakeModels/fakePosts';
 import requireParams from '../../../utils/params/requireParams';
 import validParams from '../../../utils/params/validParams';
 import setupPagination from '../../../utils/pagination/setupPagination';
@@ -11,11 +11,49 @@ describe('service - posts', () => {
     describe('findAll', () => {
         test('should return a list of posts', async () => {
             const result = await service.findAll({});
-            console.log(result);
-            
             const expected = {
                 posts: mockPostsLits.map(
                     ({ _id, title, cover, description, slug, }) => ({ id: _id, title, cover, description, slug })),
+                pagination: {
+                    page: 1,
+                    pages: 1,
+                    total: 2,
+                },
+            }
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('findByAuthor', () => {
+        test('should return a list of posts', async () => {
+            const result = await service.findByAuthor(mockPost._id, {});
+            const expected = {
+                posts: mockPostsLits.map(
+                    ({
+                        _id,
+                        title,  
+                        cover,
+                        body,
+                        description,
+                        slug,
+                        keywords,
+                        views,
+                        timeShared,
+                        likes,
+                        isPublic,
+                    }) => ({
+                        id: _id,
+                        title,
+                        cover,
+                        body,
+                        description,
+                        slug,
+                        keywords,
+                        views,
+                        timeShared,
+                        likes,
+                        isPublic,
+                    })),
                 pagination: {
                     page: 1,
                     pages: 1,
